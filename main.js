3c8818c42b6dec6e4115b14110034b5f3c9e99ac
robertmullin <bobmullin@outlook.com>
2018-01-23 18:59:11 -0500

    Gallery
    Gallery finished, just need to add real pictures

--- main.js
+++ main.js
@@ -1,44 +1,29 @@
-// Open the Modal
-function openModal() {
-    document.getElementById('myModal').style.display = "block";
-}
 
-// Close the Modal
-function closeModal() {
-    document.getElementById('myModal').style.display = "none";
-}
+// Gallery Styles 
 
-var slideIndex = 1;
-showSlides(slideIndex);
+const current = document.querySelector("#current");
 
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
+const imgs = document.querySelectorAll(".imgs img");
+const opacity = 0.6;
 
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
+// Set first img opacity
+imgs[0].style.opacity = opacity;
 
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption");
-    if (n > slides.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    captionText.innerHTML = dots[slideIndex - 1].alt;
+imgs.forEach(img => img.addEventListener("click", imgClick));
+
+function imgClick(e) {
+  // Reset the opacity
+  imgs.forEach(img => (img.style.opacity = 1));
+
+  // Change current image to src of clicked image
+  current.src = e.target.src;
+
+  // Add fade in class
+  current.classList.add("fade-in");
+
+  // Remove fade-in class after .5 seconds
+  setTimeout(() => current.classList.remove("fade-in"), 500);
+
+  // Change the opacity to opacity var
+  e.target.style.opacity = opacity;
 }
